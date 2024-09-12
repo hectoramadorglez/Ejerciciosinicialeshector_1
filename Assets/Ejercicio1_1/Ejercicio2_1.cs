666378a700b67d923944c91a7a00311369899f26
hectoramadorglez <hectoramador2005@gmail.com>
2024-09-12 19:22:40 +0200

    ejercicios

---
+++ Assets/Ejercicio1_1/Ejercicio2_1.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_1 : MonoBehaviour
+{
+    int edad = 8;
+    int codigoPostal = 82009;
+    float altura = 1.75f;
+    char genero = 'H';
+    string nombre = "Hector";
+    int numeroDeHijos = 2;
+    float iva = 34.37f;
+    int tallacamisa = 32;
+    float peso = 75.23f;
+    float precio = 23.3f;
+    bool alumnoRepetidor = false;
+    string mensaje = "hola que tal";
+    char letra = ' ';
+    int mayorEdad = 21;
+    int minutos = 34;
+    string dia = "sabado";
+    string matriculaCoche = "45547H";
+    float contador = 23.56f;
+    bool mayorDeEdad = false;
+    char tallaCamiseta = 'M';
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ejercicio1_1/Ejercicio2_2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_2 : MonoBehaviour
+{
+    int vidas = 4;
+    float exp = 1.0f;
+    char carac = 'a';
+    float resultadoSuma = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(vidas);
+        Debug.Log(exp);
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
