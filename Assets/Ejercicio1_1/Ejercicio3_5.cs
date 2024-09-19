98cb600c339cc5c9c4969c38a6a1e9aa83a59c49
hectoramadorglez <hectoramador2005@gmail.com>
2024-09-19 19:18:36 +0200

    jhbnm

--- Assets/Ejercicio1_1/Ejercicio3_5.cs
+++ Assets/Ejercicio1_1/Ejercicio3_5.cs
@@ -6,12 +6,16 @@ public class Ejercicio3_5 : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] float radio;
+    float PI = 3.14f;
     
 
 
     void Start()
     {
-       
+        float circunferencia = 2 * PI * radio;
+        float areaDeLaCircunferencia = PI * radio * radio;
+        Debug.Log("El resulado de la circunferencia" +  circunferencia);
+        Debug.Log("El resulado de la circunferencia" + areaDeLaCircunferencia);
     }
 
     // Update is called once per frame
